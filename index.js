c4910de9854503dd30dbc80d6e4f026baed59cca
Robin Wieruch <wrobin@gmx.net>
2020-05-27 14:07:15 +0200

    permissions

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('Hello Deno');
+const url = 'http://hn.algolia.com/api/v1/search?query=javascript';
+
+fetch(url)
+  .then((result) => result.json())
+  .then((result) => {
+    const stories = result.hits.map((hit) => ({
+      title: hit.title,
+      url: hit.url,
+      createdAt: hit.created_at_i,
+    }));
+
+    console.log(stories);
+  });
