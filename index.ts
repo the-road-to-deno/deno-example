594c9f23c31f54ce1924d660e37fecebc20528dd
Robin Wieruch <wrobin@gmx.net>
2020-05-27 16:16:09 +0200

    env

--- index.ts
+++ index.ts
@@ -1,10 +1,13 @@
 import { serve } from 'https://deno.land/std/http/server.ts';
+import { config } from 'https://deno.land/x/dotenv/mod.ts';
 
 import { mapStory } from './stories.ts';
 
 const url = 'http://hn.algolia.com/api/v1/search?query=javascript';
 
-const server = serve({ port: 8000 });
+const server = serve({
+  port: parseInt(config()['PORT']),
+});
 
 for await (const req of server) {
   const result = await fetch(url).then((result) => result.json());
