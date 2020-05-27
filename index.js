72b7ad94ebf7766e3aae285d42a2f24fa1b1c43b
Robin Wieruch <wrobin@gmx.net>
2020-05-27 14:13:41 +0200

    compatibility

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const url = 'http://hn.algolia.com/api/v1/search?query=javascript';
 
-fetch(url)
-  .then((result) => result.json())
-  .then((result) => {
-    const stories = result.hits.map((hit) => ({
-      title: hit.title,
-      url: hit.url,
-      createdAt: hit.created_at_i,
-    }));
+const result = await fetch(url).then((result) => result.json());
 
-    console.log(stories);
-  });
+const stories = result.hits.map((hit) => ({
+  title: hit.title,
+  url: hit.url,
+  createdAt: hit.created_at_i,
+}));
+
+console.log(stories);
