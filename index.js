ac36cc8620da9372a365a609451f35008954245e
Robin Wieruch <wrobin@gmx.net>
2020-05-27 15:34:57 +0200

    importing

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { serve } from 'https://deno.land/std/http/server.ts';
-import format from 'https://deno.land/x/date_fns/format/index.js';
+
+import { mapStory } from './stories.js';
 
 const url = 'http://hn.algolia.com/api/v1/search?query=javascript';
 
@@ -8,14 +9,7 @@ const server = serve({ port: 8000 });
 for await (const req of server) {
   const result = await fetch(url).then((result) => result.json());
 
-  const stories = result.hits.map((hit) => ({
-    title: hit.title,
-    url: hit.url,
-    createdAt: format(
-      new Date(hit.created_at_i * 1000),
-      'yyyy-MM-dd'
-    ),
-  }));
+  const stories = result.hits.map(mapStory);
 
   req.respond({ body: JSON.stringify(stories) });
 }
