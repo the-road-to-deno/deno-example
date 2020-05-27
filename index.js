15bd89ae2fa5045fd06768c6918bc147d78dfef6
Robin Wieruch <wrobin@gmx.net>
2020-05-27 15:11:53 +0200

    srd

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
+// const url = 'http://hn.algolia.com/api/v1/search?query=javascript';
+
+// const result = await fetch(url).then((result) => result.json());
+
+// const stories = result.hits.map((hit) => ({
+//   title: hit.title,
+//   url: hit.url,
+//   createdAt: hit.created_at_i,
+// }));
+
+// console.log(stories);
+
+import { serve } from 'https://deno.land/std/http/server.ts';
+
 const url = 'http://hn.algolia.com/api/v1/search?query=javascript';
 
-const result = await fetch(url).then((result) => result.json());
+const server = serve({ port: 8000 });
 
-const stories = result.hits.map((hit) => ({
-  title: hit.title,
-  url: hit.url,
-  createdAt: hit.created_at_i,
-}));
+for await (const req of server) {
+  const result = await fetch(url).then((result) => result.json());
 
-console.log(stories);
+  const stories = result.hits.map((hit) => ({
+    title: hit.title,
+    url: hit.url,
+    createdAt: hit.created_at_i,
+  }));
+
+  req.respond({ body: JSON.stringify(stories) });
+}
